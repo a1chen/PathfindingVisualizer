09c80ec0c66f713fd5a416f0ba36abe3bd2f0562
Aaron Chen <a1chen@ucsd.edu>
2020-04-21 18:08:42 -0700

    Got start and end positions to appear

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Node from "./Node/Node";
-import "./PathfindingVisualizer.jsx";
+import "./PathfindingVisualizer.css";
 
 export default class PathfindingVisualizer extends Component {
   constructor(props) {
@@ -11,11 +11,20 @@ export default class PathfindingVisualizer extends Component {
   }
 
   componentDidMount() {
+    //Creates vector containing each row
     const nodes = [];
     for (let row = 0; row < 20; row++) {
+      //Fill each row with individual nodes
       const currentRow = [];
       for (let col = 0; col < 50; col++) {
-        currentRow.push([]);
+        const currentNode = {
+          col,
+          row,
+          //If these row/col then it is a start/finish
+          isStart: row === 10 && col === 5,
+          isFinish: row === 10 && col === 45,
+        };
+        currentRow.push(currentNode);
       }
       nodes.push(currentRow);
     }
@@ -30,10 +39,17 @@ export default class PathfindingVisualizer extends Component {
       <div className="grid">
         {nodes.map((row, rowIdx) => {
           return (
-            <div>
-              {row.map((node, nodeIdx) => (
-                <Node></Node>
-              ))}
+            <div key={rowIdx}>
+              {row.map((node, nodeIdx) => {
+                const { isStart, isFinish } = node;
+                return (
+                  <Node
+                    key={nodeIdx}
+                    isStart={isStart}
+                    isFinish={isFinish}
+                  ></Node>
+                );
+              })}
             </div>
           );
         })}
